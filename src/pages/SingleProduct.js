b8d3b5a5f8cb6d5ccfc4a820970e68072736756a
Irshad Khan <webdev727@gmail.com>
2023-07-18 03:16:57 +0530

    react-routing-play

--- src/pages/SingleProduct.js
+++ src/pages/SingleProduct.js
@@ -1,7 +1,18 @@
+import { Link, useParams } from "react-router-dom";
+import products from "../data";
+
 const SingleProduct = () => {
+  const { productId } = useParams();
+  const product = products.find((product) => product.id == productId);
+  const { name, image } = product;
+
   return (
-    <section className='section product'>
+    <section className="section product">
+      <h3>{productId}</h3>
       <h2>single product</h2>
+      <img src={image} alt={name} />
+      <h5>{name}</h5>
+      <Link to="/products">Back to Products</Link>
     </section>
   );
 };
